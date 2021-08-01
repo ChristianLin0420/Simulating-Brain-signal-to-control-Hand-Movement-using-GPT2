c8c71672e326717cecc740bb43c851e12b082964
ChristianLin0420 <crlc112358@gmail.com>
2021-08-01 11:38:16 +0800

    added visualization, random vector generator, refactor command

--- model/model_utils.py
+++ model/model_utils.py
@@ -10,31 +10,25 @@ from .gpt2wgan import gpt2wgan
 
 def load_model(path, model_name: str = "gpt2gan", noise_len: int = 784, noise_dim: int = 32):
 
-    model_path = path + '.h5'
-    config_path = path + '.txt'
+    config = load_model_config(path + '.txt')
 
-    if not os.path.exists(model_path):
-        error("Invalid model path")
+    if config is None:
+        error("Cannot get model config from given path")
     else:
-        config = load_model_config(config_path)
-
-        if config is None:
-            error("Cannot get model config from given path")
+        if model_name == "gpt2gan":
+            model = gpt2gan(config, noise_len, noise_dim)
+            model.load_weights(path)
+            weights = model.get_weights()  
+            model.set_weights(weights)
+            return model
+        elif model_name == "gpt2wgan":
+            model = gpt2wgan(config, noise_len, noise_dim)
+            model.load_weights(path)
+            weights = model.get_weights()  
+            model.set_weights(weights)
+            return model
         else:
-            if model_name == "gpt2gan":
-                model = gpt2gan(config, noise_len, noise_dim)
-                model.load_weights(model_path)
-                weights = model.get_weights()  
-                model.set_weights(weights)
-                return model
-            elif model_name == "gpt2wgan":
-                model = gpt2wgan(config, noise_len, noise_dim)
-                model.load_weights(model_path)
-                weights = model.get_weights()  
-                model.set_weights(weights)
-                return model
-            else:
-                error("Given model name is invalid")
-                return None
+            error("Given model name is invalid")
+            return None
 
-        return None
+    return None
