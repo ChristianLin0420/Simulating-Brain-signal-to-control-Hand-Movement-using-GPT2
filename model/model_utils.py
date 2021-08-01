1af042606dce560afa04403b2ce5d817664ad904
ChristianLin0420 <crlc112358@gmail.com>
2021-08-01 10:58:47 +0800

    added visualization, random vector generator, refactor command

--- model/model_utils.py
+++ model/model_utils.py
@@ -8,23 +8,26 @@ from .gpt2wgan import gpt2wgan
 
 
 
-def load_model(model_path, model_name: str = "gpt2gan", noise_len: int = 784, noise_dim: int = 32):
+def load_model(path, model_name: str = "gpt2gan", noise_len: int = 784, noise_dim: int = 32):
+
+    model_path = path + '.h5'
+    config_path = path + '.txt'
 
     if not os.path.exists(model_path):
         error("Invalid model path")
     else:
-        config = load_model_config(model_path)
+        config = load_model_config(config_path)
 
         if config is None:
             error("Cannot get model config from given path")
         else:
             if model_name == "gpt2gan":
                 model = gpt2gan(config, noise_len, noise_dim)
-                model.load_weights(model_path + '.h5')
+                model.load_weights(model_path)
                 return model
             elif model_name == "gpt2wgan":
                 model = gpt2wgan(config, noise_len, noise_dim)
-                model.load_weights(model_path + '.h5')
+                model.load_weights(model_path)
                 return model
             else:
                 error("Given model name is invalid")
