a18f36dcd66086e7e483e21a1c01e9cec1fbbc0d
ChristianLin0420 <crlc112358@gmail.com>
2021-08-16 21:47:47 +0800

    added 3d input for gpt2 model

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 
 
-import os
-import gdown
-import tarfile
+# import os
+# import gdown
+# import tarfile
 
-from numpy.random.mtrand import rand
+# from numpy.random.mtrand import rand
 
-output = 'test.npz'
+# output = 'test.npz'
 
-def extract(tar_url, extract_path='.'):
-    tar = tarfile.open(output, 'r')
-    for item in tar:
-        tar.extract(item, extract_path)
-        if item.name.find(".tgz") != -1 or item.name.find(".tar") != -1:
-            extract(item.name, "./" + item.name[:item.name.rfind('/')])
+# def extract(tar_url, extract_path='.'):
+#     tar = tarfile.open(output, 'r')
+#     for item in tar:
+#         tar.extract(item, extract_path)
+#         if item.name.find(".tgz") != -1 or item.name.find(".tar") != -1:
+#             extract(item.name, "./" + item.name[:item.name.rfind('/')])
 
 # if not os.path.exists(output):
 #     url = 'https://drive.google.com/file/d/1-WyVlbk7njGie4FK6iM_ghuPcRHIrxCg/view?usp=sharing'
@@ -21,13 +21,13 @@ def extract(tar_url, extract_path='.'):
 # else:
 #     extract(output)
 
-import numpy as np
-import matplotlib.pyplot as plt
-data = np.load('/Users/christianlin/Desktop/res/010002/010002_EC.npz', allow_pickle = True)
+# import numpy as np
+# import matplotlib.pyplot as plt
+# data = np.load('/Users/christianlin/Desktop/res/010002/010002_EC.npz', allow_pickle = True)
 
-print(data["left"].shape)
-print(data["right"].shape)
-print(data['left'][0][0][:10])
+# print(data["left"].shape)
+# print(data["right"].shape)
+# print(data['left'][0][0][:10])
 
 # random = np.random.random([28, 28, 3])
 # max = np.max(random)
@@ -40,9 +40,32 @@ print(data['left'][0][0][:10])
 # plt.show()
 
 
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
 
 
+def dataset_np(last_dim: int = 1):
+    (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+    train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
+    if last_dim == 3:
+        train_images = np.repeat(train_images, 3, axis = 3)
+
+    train_labels = keras.utils.to_categorical(train_labels, 10)
+
+    return (train_images, train_labels)
+
+
+dataset = dataset_np(3)
+
+label = dataset[1][:5]
+print(label)
+one_hot_labels = np.expand_dims(label, axis = 1)
+print(one_hot_labels)
+one_hot_labels = np.repeat(one_hot_labels, repeats = 10, axis = 1)
+print(one_hot_labels)
 
 
 
