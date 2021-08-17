e8d5d0a1137bdc3c15a7d01943da8c16d751f2c8
ChristianLin0420 <crlc112358@gmail.com>
2021-08-17 20:51:01 +0800

    refactor 3 dimensional conditional gan

--- model/gpt2cgan.py
+++ model/gpt2cgan.py
@@ -74,6 +74,8 @@ class gpt2cgan(tf.keras.Model):
         image_size_h = real_images.shape[1]
         image_size_w = real_images.shape[2]
         num_classes = real_images.shape[-1]
+        print(image_size_h)
+        print(image_size_w)
 
         # one hot information
         one_hot_labels = tf.expand_dims(real_labels, axis = 1)
@@ -84,6 +86,7 @@ class gpt2cgan(tf.keras.Model):
         image_one_hot_labels = tf.repeat(
             image_one_hot_labels, repeats = [image_size_h * image_size_w]
         )
+        print(image_one_hot_labels.shape)
         image_one_hot_labels = tf.reshape(
             image_one_hot_labels, (-1, image_size_h, image_size_w, num_classes)
         )
