7af682db0c8db1b82ead31abdf76ec2ad7954cd8
ChristianLin0420 <crlc112358@gmail.com>
2021-08-23 11:25:19 +0800

    added distribution generator

--- train.py
+++ train.py
@@ -278,7 +278,7 @@ def training(args, datasets, time, num_classes: int = 2):
                         x = datasets[0],
                         y = datasets[1],
                         batch_size = batch_size,
-                        epochs = int(1), 
+                        epochs = int(args.epochs), 
                         verbose = 1, 
                         callbacks = [EarlyStoppingAtMinLoss(), RecordGeneratedImages(time, current_round, args.model)]#, tensorboard_callback]
                     )
