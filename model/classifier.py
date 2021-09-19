46a7771cf335764c8274f34d3be134e97386bffc
ChristianLin0420 <crlc112358@gmail.com>
2021-09-19 20:24:28 +0800

    fix classifier model

--- model/classifier.py
+++ model/classifier.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 
-def get_pretrained_classfier(path = '/home/jupyter-ivanljh123/Simulating-Brain-signal-to-control-Hand-Movement-using-GPT2/pretrained/09_0.92.h5'):
+def get_pretrained_classfier(path = '/home/jupyter-ivanljh123/Simulating-Brain-signal-to-control-Hand-Movement-using-GPT2/pretrained/09_0.92'):
     #load pretrained model
     model = tf.keras.models.load_model(path)
     model.trainable = False
