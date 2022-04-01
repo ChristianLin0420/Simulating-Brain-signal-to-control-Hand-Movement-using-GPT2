7a05c6ee1f7277334a9c9a10ca7d8a52d2de3e4a
ChristianLin0420 <crlc112358@gmail.com>
2022-04-02 01:02:06 +0800

    fix issue

--- modules/gpt2xCNN.py
+++ modules/gpt2xCNN.py
@@ -122,8 +122,10 @@ class gpt2xcnn(tf.keras.Model):
             else:
                 XX = tf.concat([XX, X], axis = 0)
 
+            print(X.shape)
 
-            y_pred = tf.cast(tf.math.argmax(self.classifier(X), axis = 1), tf.int64)
+            y_pred = self.classifier(X)
+            print(type(y_pred))
             y_true = labels[idx * generate_count: (idx + 1) * generate_count]
 
             loss = self.loss_fn(y_true, y_pred)
