63540fbf6273945386ed67adc149161c87af68b4
ChristianLin0420 <crlc112358@gmail.com>
2022-04-02 01:20:17 +0800

    fix issue

--- modules/gpt2xCNN.py
+++ modules/gpt2xCNN.py
@@ -127,11 +127,8 @@ class gpt2xcnn(tf.keras.Model):
             y_pred = tf.reshape(y_pred, [generate_count, 1])
             y_pred = tf.cast(y_pred, tf.int64)
             y_true = labels[idx * generate_count: (idx + 1) * generate_count]
-            y_true = tf.constant(y_true)
             print(y_pred.shape)
             print(type(y_pred))
-            print(y_true.shape)
-            print(type(y_true))
 
             loss = self.loss_fn(y_true, y_pred)
 
