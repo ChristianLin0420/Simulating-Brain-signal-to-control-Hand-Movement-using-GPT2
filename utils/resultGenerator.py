b6528f763145accfad29bbe77918065bf85f6d48
ChristianLin0420 <crlc112358@gmail.com>
2022-04-11 17:36:33 +0800

    add topograph and eeg functions

--- utils/resultGenerator.py
+++ utils/resultGenerator.py
@@ -9,6 +9,7 @@ from res.brain import Brain
 
 from .brain_activation import (
     boolean_brain, 
+    fetch_brain_template,
     transformation_matrix, 
     restore_brain_activation, 
     restore_brain_activation_tf, 
@@ -23,8 +24,9 @@ class ResultGenerator(object):
 
         (self.boolean_l, self.boolean_r) = boolean_brain()
         self.transformation_matrix = transformation_matrix()
+        self.brain_template = fetch_brain_template()
 
-    def generate_figure(self, data, label, title):
+    def generate_figure(self, data, label, title, path):
         lst_iter = np.arange(self.config.epochs)
 
         if data.ndim == 3:
@@ -48,7 +50,7 @@ class ResultGenerator(object):
         plt.title(title)
 
         # save image
-        plt.savefig("result/{}/{}/figure/{}.png".format(self.config.model_name, self.time, title))  
+        plt.savefig(path)  
         plt.close()
 
     def generate_training_result_figure(self):
@@ -65,38 +67,63 @@ class ResultGenerator(object):
                 datas.append(data)
 
             datas = np.asarray(datas)
-            self.generate_figure(datas, [filename], filename)
+            self.generate_figure(datas, [filename], filename, "result/{}/{}/figure/{}.png".format(self.config.model_name, self.time, filename))
 
-    def generate_all_channels_eeg(self):
+    def generate_all_channels_eeg(self, data, epoch):
         channels = Brain.get_channel_names()
         real_data = np.asarray(self.real_data)
         t_matrix = np.asarray(self.transformation_matrix)
-        real_converted_data = np.dot(t_matrix, real_data)
 
-        for r in range(self.config.rounds):
-            for e in range(0, self.config.epochs, 10):
-                filename = "result/{}/{}/generated/{}/{}/generated.json".format(self.config.model_name, self.time, r, e)
-                data = json.load(filename)
-                data = data["generated"]
-                generated_num_per_class = int(self.config.example_to_generate / self.config.class_count)
-                left, right = restore_brain_activation(data, self.boolean_l, self.boolean_r)
-                left = np.asarray(left)
-                right = np.asarray(right)
-                data = np.concatenate([left, right], axis = 0)
-                data = np.dot(t_matrix, data)
+        generated_num_per_class = int(self.config.example_to_generate / self.config.class_count)
 
-                for i in range(data.shape[0]):
-                    wave1 = np.expand_dims(real_converted_data[i], axis = 0)
-                    wave2 = np.expand_dims(data[i], axis = 0)
-                    waves = np.concatenate([wave1, wave2])
-                    labels = ["real_data_channel_{}".format(channels[i]), "generated_data_channel_{}".format(channels[i])]
+        for i in range(self.config.class_count):
+            class_data = np.mean(data[i * generated_num_per_class : (i + 1) * generated_num_per_class], axis = 0).tolist()
+            left, right = restore_brain_activation(class_data, self.boolean_l, self.boolean_r)
+            left = np.asarray(left)
+            right = np.asarray(right)
+            class_data = np.concatenate([left, right], axis = 0)
+            class_data = np.dot(t_matrix, class_data)
+            real_converted_data = np.dot(t_matrix, real_data[i])
 
-                    self.generate_figure(waves, labels, "Channel_{}_epoch_{}".format(channels[i], e))
+            if not os.path.exists("result/{}/{}/eeg/{}/epoch_{:04d}".format(self.config.model_name, self.time, self.round, epoch)):
+                os.mkdir("result/{}/{}/eeg/{}/epoch_{:04d}")
 
+            for c in range(data.shape[0]):
+                wave1 = np.expand_dims(real_converted_data[c], axis = 0)
+                wave2 = np.expand_dims(class_data[c], axis = 0)
+                waves = np.concatenate([wave1, wave2])
+                labels = ["real_data_channel_{}".format(channels[c]), "generated_data_channel_{}".format(channels[c])]
 
+                self.generate_figure(waves, labels, "class_{}_channel_{}_epoch_{}".format(i, channels[c], epoch), "result/{}/{}/eeg/{}/epoch_{:04d}/class_{}_{}.png".format(self.config.model_name, self.time, self.round, epoch, i, channels[c]))
 
-    def generate_topographs(self):
-        pass
+    def generate_topographs(self, data, epoch):
+        real_data = np.asarray(self.real_data)
+        t_matrix = np.asarray(self.transformation_matrix)
+
+        generated_num_per_class = int(self.config.example_to_generate / self.config.class_count)
+
+        for i in range(self.config.class_count):
+            class_data = np.mean(data[i * generated_num_per_class : (i + 1) * generated_num_per_class], axis = 0).tolist()
+            left, right = restore_brain_activation(class_data, self.boolean_l, self.boolean_r)
+            left = np.asarray(left)
+            right = np.asarray(right)
+            class_data = np.concatenate([left, right], axis = 0)
+            class_data = np.dot(t_matrix, class_data)
+            real_converted_data = np.dot(t_matrix, real_data[i])
+
+            if not os.path.exists("result/{}/{}/mne/{}/epoch_{:04d}".format(self.config.model_name, self.time, self.round, epoch)):
+                os.mkdir("result/{}/{}/mne/{}/epoch_{:04d}")
+
+            self.brain_template.data = class_data
+            ax = self.brain_template.plot_topomap(times = np.linspace(0.0, 0.2, 20), ch_type = 'eeg', time_unit='s', ncols=5, nrows='auto', title = 'Original Class_{} Brain Activation in iteration {}'.format(i, epoch), show = False)
+            ax.savefig("result/{}/{}/mne/{}/epoch_{:04d}/original_class_{}_topograph.png".format(self.config.model_name, self.time, self.round, epoch, i))
+            plt.close(ax)
+
+            self.brain_template.data = real_converted_data
+            ax = self.brain_template.plot_topomap(times = np.linspace(0.0, 0.2, 20), ch_type = 'eeg', time_unit='s', ncols=5, nrows='auto', title = 'Generated Class_{} Brain Activation in iteration {}'.format(i, epoch), show = False)
+            ax.savefig("result/{}/{}/mne/{}/epoch_{:04d}/generated_class_{}_topograph.png".format(self.config.model_name, self.time, self.round, epoch, i))
+            plt.close(ax)
+
 
     def generate_stft(self, data, epoch):
         brain = None
