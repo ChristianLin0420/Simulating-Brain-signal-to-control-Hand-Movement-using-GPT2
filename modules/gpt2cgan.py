57d89339f5b031ff86fee172efac48c9acb48d3e
ChristianLin0420 <crlc112358@gmail.com>
2022-04-16 15:57:38 +0800

    finish generate eeg & mne

--- modules/gpt2cgan.py
+++ modules/gpt2cgan.py
@@ -136,7 +136,7 @@ class gpt2cgan(tf.keras.Model):
         d_loss = 0
 
         for _ in range(self.d_extra_steps):
-            (random_latent_vectors, _, _) = generate_random_vectors_with_labels(_real_labels, self.generate_count, self.config.n_positions, self.config.n_embd, self.config.class_rate_random_vector, self.config.class_count, self.config.noise_variance)
+            random_latent_vectors = generate_random_vectors_with_labels(_real_labels, self.generate_count, self.config.n_positions, self.config.n_embd, self.config.class_rate_random_vector, self.config.class_count, self.config.noise_variance)
             
             # generate images from gpt2 
             generated_images = self.generator(random_latent_vectors)
